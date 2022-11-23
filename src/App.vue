dd2ba56150d74ae87528317f224e81537b85c78e
compilekaiten <103348829+compilekaiten@users.noreply.github.com>
2022-11-22 18:41:10 -0800

    - Add UseHead SEOMeta Tags - Dep up

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useSeoMeta } from '@vueuse/head'
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
@@ -21,6 +22,12 @@ useHead({
       href: '/favicon.ico',
     },
   ],
+})
+
+useSeoMeta({
+  description: 'ViteTail - Vite Starter Template',
+  ogDescription: 'ViteTail - A Vite/Vue3/TailwindCSS SSG starter template.',
+  ogTitle: 'ViteTail',
 })
 </script>
 
